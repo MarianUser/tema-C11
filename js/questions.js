1456c6b555845f93797bcc8a242025feeed61b57
MarianUser <117512066+MarianUser@users.noreply.github.com>
2023-01-14 19:29:14 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+//creeating an array and passing the number, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Ce este un algoritm?",
+        answer: "B. Un set de pasi, repetati pentru a gasi rezultatul",
+        options: [
+            "A. O problema de rezolvat cu ajutorul calculatorului",
+            "B. Un set de pasi, repetati pentru a gasi rezultatul",
+            "C. O materie de liceu",
+            "D. N-am auzit niciodata de acest cuvant"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Ce putem afirma atunci cand clasa B mosteneste clasa A?",
+        answer: "B. Clasa B ia toate metodele si campurile clasei A",
+        options: [
+            "A. Clasa A ia toate campurile si metodele clasei B",
+            "B. Clasa B ia toate metodele si campurile clasei A",
+            "C. Clasa B devine o copie a clasei A",
+            "D. N-am auzit niciodata de clase"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Care sunt functiile principale ale unui server?",
+        answer: "A. Gestioneaza request-urile si serveste resursele statice",
+        options: [
+            "A. Gestioneaza request-urile si serveste resursele statice",
+            "B. Ne ajuta sa ne jucam pe Internet",
+            "C. Este un computer care ruleaza 24/7",
+            "D. N-am auzit niciodata de acest cuvant"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Ce serviciu este potrivit pentru a gestiona cod intr-o echipa?",
+        answer: "C. GitHub",
+        options: [
+            "A. Maven",
+            "B. Cucumber",
+            "C. GitHub",
+            "D. Foaia de hartie si pixul"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Care este Package Manager-ul limbajului Python?",
+        answer: "A. pip",
+        options: [
+            "A. pip",
+            "B. py",
+            "C. apt",
+            "D. dhl"
+        ]
+    },
+]
